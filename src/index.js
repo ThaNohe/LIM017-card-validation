eb02c9c553aa22eabbe66abc1f4ab018dc90bbd8
Thalía Nohelia <thalia.cuadros.bautista@gmail.com>
2022-02-22 10:31:23 -0500

    Validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-import validator from './validator.js';
+//Importando el mensaje 
+import validator from "./validator.js";
+const msg = document.getElementById('mensaje').innerHTML;
+console.log(msg);
 
-console.log(validator);
+const validateInformation = document.getElementById('btn');
+validateInformation.addEventListener('click', validarCardNumber );
+
+function validarCardNumber(){
+    let cardNumber=document.getElementById('cardNumber').value;
+    let maskedCardNumber = validator.maskify(cardNumber);
+    console.log(maskedCardNumber);
+
+    if(cardNumber==''){
+        document.getElementById('mensaje').innerHTML='Por favor ingresar número de tarjeta válida';
+    }
+    else{
+        document.getElementById('mensaje').innerHTML='Información de tarjeta válida';   
+        validator.isValid(cardNumber)
+        
+    }
+    
+    
+}
+
+
+
+//function isCorrectCardNumber(dato){
+//    let patron = /^(?:(4[0-9]{12}(?:[0-9]{3})?)|(5[1-5][0-9]{14})|(6(?:011|5[0-9]{2})[0-9]{12})|(3[47][0-9]{13})|(3(?:0[0-5]|[68][0-9])[0-9]{11})|((?:2131|1800|35[0-9]{3})[0-9]{11}))$/;
+//   return patron.test(dato);
+//}
+
+
+//let  inputCardNumber=
+
+//const paintValidate = () => {
+//    alertSuccess.textContent = "¡Proceso exitoso!"
+//}
+//paintValidate();
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,64 @@
 const validator = {
-  // ...
-};
+   isValid: function (number) {
+      //Algoritmo Luhn:
+      //Convertir los inputs ingresados por el usuario a Array y revertirlo
+      let myArray = Array.from(number);
+      console.log("mi numero ingresado es " + myArray);
+      let myArrayReverse = myArray.reverse();
+      console.log("mi numero revertido es " + myArrayReverse);
 
+      //Crear variables Suma posiciones pares y posiciones impares
+      let sumaPares = 0;
+      let sumaImpares = 0;
+
+      for (let i = 0; i < myArrayReverse.length; i++) {
+         //console.log(typeof myArrayReverse[i])
+         if (i % 2 !== 0) {
+            console.log("entre al if");
+            let positionPar = parseInt(myArrayReverse[i])* 2;
+            //console.log(typeof positionPar);
+            console.log("mi numero par multiplicado por 2 es " + positionPar);
+           if (positionPar >= 10) {
+              positionPar=positionPar-9;
+               console.log(positionPar);
+            }
+            sumaPares =sumaPares+positionPar;    
+         }
+         else {
+               let positionImpar = parseInt(myArrayReverse[i]);
+               sumaImpares=sumaImpares+positionImpar;
+         }
+         }
+         console.log('Esta es la suma de los números pares:' + sumaPares);
+         console.log('Esta es la suma de los números impares:' + sumaImpares);
+         let sumaTotal=sumaPares+sumaImpares;
+         if(sumaTotal % 10 ==0){
+            console.log('válido');
+            return true;
+         }
+         else{
+            console.log('no válido')
+            return false;
+         }
+      },
+     maskify: function (cardNumber) {
+      //EnmascararDígitos
+      //Crear variables para recorrido
+      let maskedCardNumber=[];
+  //console.log(maskedCardNumber);
+  for (let i = 0; i < cardNumber.length; i++)
+  //console.log(cardNumber.length);
+   {
+     if (i < cardNumber.length - 4) 
+     console.log(cardNumber.length - 4){
+        maskedCardNumber.push("#");
+      } else {
+         maskedCardNumber.push(cardNumber[i]);
+
+      }
+   } 
+    return maskedCardNumber.join('');
+   console.log(maskedCardNumber)
+ }
+}
 export default validator;
